da47c4ba9a5b7e4195ff7f7cd17482921184b2db
huyhoangng <133932405+huyhoangng@users.noreply.github.com>
2023-09-05 09:47:01 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿Create view vwPerson 
+AS
+SELECT e1.BusinessEntityID, e1.BirthDate, e2.Rate, JobTitle
+From HumanResources.Employee e1
+JOIN HumanResources.EmployeePayHistory e2
+ON e1.BusinessEntityID = e2.BusinessEntityID
+select * from [HumanResources].[Employee]
+select * from HumanResources.EmployeePayHistory
+INSERT INTO vwPerson VALUES (2,'2000-10-12','150.50','Ăn chơi nhảy múa')
